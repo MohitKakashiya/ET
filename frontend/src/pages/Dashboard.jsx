f38a2baad964a722c674524e5030d21112c4cfae
MohitKakashiya <mkmohit0916@gmail.com>
2026-05-26 14:24:48 +0530

    Update Dashboard.jsx
    For using render

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -45,7 +45,7 @@ import {
 } from "recharts";
 import FinancialCard from "../components/FinancialCard";
 import GaugeCard from "../components/GaugeCard";
-const API_BASE = "http://localhost:4000/api";
+const API_BASE = "https://myrevenue-backend.onrender.com/api";
 
 const getAuthHeader = () => {
   const token =
